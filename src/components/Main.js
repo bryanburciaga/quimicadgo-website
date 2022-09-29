c5912f919b92ba36a41ad53bbf2bcbe4b6d6a0d1
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-09-28 18:29:02 -0600

    files added and working

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import Index from "../pages/Index";
+import Show from "../pages/Show";
+
+function Main(props) {
+    return <h1>Main page</h1>
+}
+
+export default Main;
---
+++ src/pages/Show.js
@@ -0,0 +1,5 @@
+function Show(props) {
+    return <h1>Show page</h1>
+}
+
+export default Show;

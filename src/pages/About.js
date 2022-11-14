66f4970e9bd2fe931bb3eeddfa80b4b7f00ada16
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-11-13 20:51:59 -0700

    contact page fixed

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,44 @@
 import React from "react";
+import { useState, useEffect } from 'react'
 import '../App.css'
 import './About.css'
 
 
 function About() {
-        return (
-          <div className='about'>
-            <h1>Química Durango</h1>
-            <h2>Somos una empresa 100% duranguense!</h2>
+  const [Text, setText] = useState('About');
+  const [missionText, setMissionText] = useState('Mission');
+  const [visionText, setVisionText] = useState('Vision');
+
+  function handleAboutClick() {
+    setText('Somos una empresa Duranguense que empezó y creció en la pandemia. Nos dedicamos en la fabricación y comercialización de productos químicos y de limpieza para el hogar. Hemos trabajado con muchas empresas y hogares duranguenses para proveer los mejores productos, con la mas alta calidad y a los mejores precios.');
+  }
+  function handleMissionClick() {
+    setMissionText("hola");
+  }
+  function handleVisionClick() {
+    setVisionText("adios");
+  }
+
+
+
+
+  return (
+    <>
+      <div>
+        <button onClick={handleAboutClick}>{Text}</button>
+      </div>
+      <div>
+        <button onClick={handleMissionClick}>{missionText}</button>
+      </div>
+      <div>
+        <button onClick={handleVisionClick}>{visionText}</button>
+      </div>
+      </>
+  );
+};
+  
+            // <h1>Química Durango</h1>
+            // <h2>Somos una empresa 100% duranguense!</h2>
             <p>Somos una empresa Duranguense que empezó y creció
             en la pandemia. 
             Nos dedicamos en la fabricación y comercialización
@@ -16,8 +47,7 @@ function About() {
             duranguenses para proveer los mejores productos, con
             la mas alta calidad y a los mejores precios.
             </p>
-          </div>
-        );
-      }
+
+
 
       export default About;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,7 +8,14 @@ function Contact() {
     <h1 class name='contact'>Contacto</h1>
     <div className="row">
         <div className="contactInfo">
-        <label>HolaHolaHolaHolaHolaHolaHolaHolaHolaHola</label>
+        <div class='social-icons'>
+            <a href='https://www.facebook.com/profile.php?id=100064217254366'
+             class='social-icon-link facebook'
+             target="_blank" 
+             rel="noopener noreferrer"
+             aria-label='Facebook'>
+        <i class='fab fa-facebook-f'>Química Durango</i>
+            </a>
             <a href="https://wa.me/526183330007"
                   class='social-icon-link whatsapp'
                   target="_blank" 
@@ -16,18 +23,47 @@ function Contact() {
                   aria-label='Facebook'>
                   <i class="fa fa-whatsapp" aria-hidden="true">Whatsapp</i>
             </a>
+            <a href="https://wa.me/526183330007"
+                  class='social-icon-link whatsapp'
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  aria-label='Facebook'>
+                  <i class="fa fa-phone" aria-hidden="true">Cel: 618-333-0007</i>
+            </a>
+            ó
+            <a href="https://wa.me/526182910347"
+                  class='social-icon-link whatsapp'
+                  target="_blank" 
+                  rel="noopener noreferrer"
+                  aria-label='Facebook'>
+                  <i class="fa fa-phone" aria-hidden="true">Cel: 618-291-0347</i>
+            </a>
+            <i style= {{ color: "white" }} class="material-icons">add_location</i>
+            <a 
+                href="https://www.google.com/maps/place/C.+Negrete+418,+Zona+Centro,+34000+Durango,+Dgo.,+M%C3%A9xico/@24.0284565,-104.6576533,3a,75y,199.39h,83.8t/data=!3m6!1e1!3m4!1sIKw4Pux7JZqx0uii6kgJ1g!2e0!7i16384!8i8192!4m5!3m4!1s0x869bb7df75b878d9:0xf0e96569c5aae6dd!8m2!3d24.0283411!4d-104.6575865"
+                class='social-icon-link whatsapp'
+                target="_blank" 
+                rel="noopener noreferrer"
+                aria-label='Facebook'>
+                <i class="material-icons" aria-hidden="true"></i>Negrete 418 ote, zona centro. Durango, Durango.
+            </a>
+            
+            
+            <i class="fa fa-phone"
+             aria-label='Facebook'>618-333-0007</i>
+            </div>
         </div>
         <div className="contactForm">
-                <label>Name
-                    <input type="text" name="" id="" placeholder='Name' />
+                <label>Nombre:
+                    <input className='contactBoxes' type="text" name="" id="" placeholder='Name' />
                 </label>
-                <label>Email
-                    <input type="text" name="" id="" placeholder='Email'/>
+                <label>Email:
+                    <input className='contactBoxes' type="text" name="" id="" placeholder='Email'/>
                 </label>
-                <label>Description
-                    <input type="text" />
+                <label>Descripción:
+                    <input className='contactBox' type="text" placeholder='Descripción del producto' />
                 </label>
-                <input type="submit" />
+                <input className='submitButton' type="submit" />
         </div>
     </div>
     </>

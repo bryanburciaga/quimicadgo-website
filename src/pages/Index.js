5ea861d6180a87619f1a04b19799785580d48f52
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-11-18 15:46:40 -0700

    video added

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -5,7 +5,7 @@ import './Index.css';
 function Index() {
   return (
     <div className="video-container">
-        <video src="/videos/video-2.mp4" autoPlay loop muted />
+        <video src="/video/video1.mp4" autoPlay loop muted />
         <h1>Bienvenido a Química Durango</h1>
 
     </div>

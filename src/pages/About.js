59e703126bf212b76e7a2586413c226d3ecb7db0
Bryan Burciaga <bryanburciaga@Bryans-MacBook-Pro.local>
2022-11-18 15:32:34 -0700

    acento agregado

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,10 +6,10 @@ import './About.css'
 
 function About() {
 
-const [ text, setText ] = useState('Somos una empresa Duranguense que empezó y creció en la pandemia. Nos dedicamos en la fabricación y comercialización de productos químicos y de limpieza para el hogar. Hemos trabajado con muchas empresas y hogares duranguenses para proveer los mejores productos, con la mas alta calidad y a los mejores precios.');
+const [ text, setText ] = useState('Somos una empresa Duranguense que empezó y creció en la pandemia. Nos dedicamos en la fabricación y comercialización de productos químicos y de limpieza para el hogar. Hemos trabajado con muchas empresas y hogares duranguenses para proveer los mejores productos, con la más alta calidad y a los mejores precios.');
 
 const handleAbout = () => (
-  setText('Somos una empresa Duranguense que empezó y creció en la pandemia. Nos dedicamos en la fabricación y comercialización de productos químicos y de limpieza para el hogar. Hemos trabajado con muchas empresas y hogares duranguenses para proveer los mejores productos, con la mas alta calidad y a los mejores precios.')
+  setText('Somos una empresa Duranguense que empezó y creció en la pandemia. Nos dedicamos en la fabricación y comercialización de productos químicos y de limpieza para el hogar. Hemos trabajado con muchas empresas y hogares duranguenses para proveer los mejores productos, con la más alta calidad y a los mejores precios.')
 );
 
 const handleMission = () => (
